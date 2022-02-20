80aa5f0b148f971af25ad45375675f6e808096dd
benito <pisalseng@gmail.com>
2022-02-20 11:48:21 +0700

    mylaravel

---
+++ resources/views/member.blade.php
@@ -0,0 +1,3 @@
+
+<h1>Welcome</h1>
+<a href="<?php echo url('/');?>">HOME</a> | <a href="<?php echo route('member.show', ['id'=>14]);?>">MEMBER</a>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    //return "Hello World";
+    
+    return view('layout.main');
+});
+
+
+
+Route::get('member/{id}', 'MemberController@show')->name('member.show');
